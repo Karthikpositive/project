250c009173d562f624b13e88498b37271495681e
Karthikpositive <162668103+Karthikpositive@users.noreply.github.com>
2024-03-08 11:04:59 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,64 @@
+def function():     #Function Defination
+    a=int(input("Enter The Number:-"))
+    b=int(input("Enter The Another Number:-"))
+    #print("This is karthik")     #Function Body
+    print("Addition Of The Two Numbers:-",a+b)
+function()         
+
+
+'''def fun(a,b,c): 
+    print("good Night",a,b,c)
+fun(1,2,3)
+'''
+
+'''#orbitary aruguments or orbitary parameters
+
+def fun(*a):            #single value lo mottam ravali ante star(*)ivvali
+    print("good Night",a)      
+fun(1,2,3)
+
+#the result will come in tuple format( )'''
+
+'''#keyword arguments
+def fun(**a): 
+    print("good Night",a)
+fun(a=1,b=2,c=8)
+#the result will come dictionary format{ }
+'''
+
+'''def outer(age=18):
+    a=int(input("Enter Age:-"))
+    print("Eligible for vote")
+    def inner(age=18):
+        print("not Eligible for vote")
+    inner()
+outer()
+
+edi normal try chesaa
+
+'''
+
+
+
+'''def outer():
+    print("outer function")
+    def inner():
+        print("inner function")
+    inner()
+outer()
+'''
+
+#Import the fuction
+
+def add(a,b):
+    print(a+b)
+def sub(a,b):
+    print(a-b)
+def mul(a,b):
+    print(a*b)
+def div(a,b):
+    print(a/b)
+
+
+
+    
